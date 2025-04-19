abd62157e6b3c7d586f48bbeea87d65e2ee614c0
Oleksandr Burenok <oburenok@softserveinc.com>
2025-04-19 15:42:08 +0300

    Adding file globl.py with global variables.

---
+++ utils/globl.py
@@ -0,0 +1,43 @@
+
+
+# List of global parameters for Environment
+host = None
+url = None
+browser = None
+
+
+# List of test parameters
+test_name = None
+test_method_name = None
+test_path = None
+test_report_path = None
+test_report_file = None
+test_counters = {'total_checkpoints': 0,
+                 'total_warnings': 0,
+                 'total_errors': 0,
+                 'total_exceptions': 0}
+
+
+# List of project parameters
+project_name = 'shady-meadows-repo'
+project_path = None  # 'C:\\Users\\oburenok\\PycharmProjects\\Luma'
+reports_path = None
+driver_chrome_path = '\\drivers\\chrome\\chromedriver.exe'
+driver_firefox_path = '\\drivers\\firefox\\geckodriver.exe'
+driver_edge_path = '\\drivers\\edge\\msedgedriver.exe'
+
+
+# Logging
+logging_level = 'INFO'
+# All possible loginig levels:
+                # CRITICAL = 50
+                # FATAL = CRITICAL
+                # ERROR = 40
+                # WARNING = 30
+                # WARN = WARNING
+                # INFO = 20
+                # DEBUG = 10
+                # NOTSET = 0
+
+
+# Parameters Jenkins
