9859247fc614dffb1b6ad7c061901d5e7349bd0c
Oleksandr Burenok <oburenok@softserveinc.com>
2025-05-02 18:52:25 +0300

    Updating test tests/test_4.py

--- tests/test_4.py
+++ tests/test_4.py
@@ -18,8 +18,19 @@ def test_page_object(home_page: HomePage, login_page: LoginPage):
     # login_page.click_logout()
 
     log.step("1.003", "Entering username and password..")
-    login_page.load()
+    # login_page.load()
     # login_page.page.get_by_label("Username").fill("user")
-    login_page.page.get_by_test_id("username").fill("qwer")
+    # login_page.page.get_by_label("Password").fill("user")
     # login_page.login("user", "password")
-    time.sleep(2)
+
+    login_page.navigate_to_page("https://automationintesting.online/")
+    # login_page.page.get_by_label("Check In").fill("02/02/2026")
+    # login_page.page.get_by_label("Check In").fill("07/07/2026")
+
+    # login_page.page.get_by_label("Name").fill("user")
+    # login_page.page.get_by_label("Email").fill("user")
+    # login_page.page.get_by_label("Phone").fill("user")
+    # login_page.page.get_by_label("Subject").fill("user")
+    login_page.page.get_by_test_id("ContactDescription").fill("user")
+    # login_page.page.locator("xpath=//textarea[@id='description']").fill("bla-bla!!")
+    time.sleep(4)
