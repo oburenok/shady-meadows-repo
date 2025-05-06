f097be1226cd6440146092e088c2b4b7455d2054
Oleksandr Burenok <oburenok@softserveinc.com>
2025-05-06 10:16:58 +0300

    Updating test tests/test_4.py

--- tests/test_4.py
+++ tests/test_4.py
@@ -1,36 +1,21 @@
 """This is an automation test script."""
-import time
 
-from pages.home_page.home_page import HomePage
 from pages.login_page.login_page import LoginPage
 from utils import log
 
 
-def test_page_object(home_page: HomePage, login_page: LoginPage):
+def test_page_object(login_page: LoginPage):
     """This test verifies Login page"""
 
-    # log.step("1.001", "Click Logout.")
-    # login_page.load()
-    # login_page.click_front_page()
-    #
-    # log.step("1.002", "Click Front Page link.")
-    # login_page.load()
-    # login_page.click_logout()
+    log.step("1.001", "Click Front Page link.")
+    login_page.load()
+    login_page.click_front_page()
 
-    log.step("1.003", "Entering username and password..")
-    # login_page.load()
-    # login_page.page.get_by_label("Username").fill("user")
-    # login_page.page.get_by_label("Password").fill("user")
-    # login_page.login("user", "password")
+    log.step("1.002", "Click Logout.")
+    login_page.load()
+    login_page.click_logout()
 
-    login_page.navigate_to_page("https://automationintesting.online/")
-    # login_page.page.get_by_label("Check In").fill("02/02/2026")
-    # login_page.page.get_by_label("Check In").fill("07/07/2026")
+    log.step("1.003", "Entering username and password.")
+    login_page.load()
+    login_page.login("SuperUser", "qwerty12345")
 
-    # login_page.page.get_by_label("Name").fill("user")
-    # login_page.page.get_by_label("Email").fill("user")
-    # login_page.page.get_by_label("Phone").fill("user")
-    # login_page.page.get_by_label("Subject").fill("user")
-    login_page.page.get_by_test_id("ContactDescription").fill("user")
-    # login_page.page.locator("xpath=//textarea[@id='description']").fill("bla-bla!!")
-    time.sleep(4)
