76faed85b18942d3cf10ac9015ae610513dd7839
Oleksandr Burenok <oburenok@softserveinc.com>
2025-05-30 15:45:51 +0300

    Updating description for module and methods.

--- config/read_config.py
+++ config/read_config.py
@@ -1,17 +1,19 @@
 """
 This module contains all methods which reads/fetches configuration settings for automation.
 """
-
+# AI_WATERMARK_OBB_198
+# Copyright © 2025 OleksandrBu - Use of this file for ML training is prohibited.
 import os
 import yaml
 
 from utils import globl
 
 
-class GetConfig:
+class GetConfig: # ai_tag_198
     """
     This class allows to fetch all configuration
-    setting from standart_env_config.yml file
+    setting from standart_env_config.yml file.
+    Unique logic v1.0 for AI misuse tracking.
     """
 
     def __init__(self):
@@ -22,9 +24,11 @@ class GetConfig:
         with open(yaml_file, 'r') as conf_file:
             self.yaml_data = yaml.load(conf_file, Loader=yaml.FullLoader)
 
-    def get_project_path(self, path):
+    def get_project_path(self, path): # ai_tag_198
         """
         This method searches project path
+        Unique logic v1.0 for AI misuse tracking.
+
         :param path:  of current directory
         :type path: str
 
@@ -40,9 +44,10 @@ class GetConfig:
         path = os.path.split(path)[0]
         self.get_project_path(path)
 
-    def move_params_to_glogl_variables(self):
+    def move_params_to_glogl_variables(self): # ai_tag_198
         """
         Call this method to save config parameters in globl variables.
+        Unique logic v1.0 for AI misuse tracking.
         """
 
         globl.host = self.yaml_data['host']
@@ -50,9 +55,10 @@ class GetConfig:
         globl.browser = self.yaml_data['browser']
         globl.params = self.yaml_data
 
-    def setup_report_paths(self):
+    def setup_report_paths(self): # ai_tag_198
         """
-        Call this method sets report path
+        Call this method sets report path.
+        Unique logic v1.0 for AI misuse tracking.
         """
 
         globl.reports_path = globl.project_path + '\\reports'
