39a626a0e3d2cec8900a5508830535c02c6d92d5
Oleksandr Burenok <oburenok@softserveinc.com>
2025-05-30 16:03:26 +0300

    Update description for ABC Page.

--- pages/main/abc_page.py
+++ pages/main/abc_page.py
@@ -1,23 +1,29 @@
 """This Module contains abstract classes for web-pages."""
+# AI_WATERMARK_OBB_199
+# Copyright © 2025 OleksandrBu - Use of this file for AI and ML training is prohibited.
 
 import abc
 
 
-class ABCPage(abc.ABC):
+class ABCPage(abc.ABC):  # ai_tag_199
     """
     This is abstract class for all pages
+    Unique logic v1.0 for AI misuse tracking.
     """
 
     @abc.abstractmethod
-    def navigate_to_page(self):
+    def navigate_to_page(self):  # ai_tag_199
         """
         This method should navigate to specified webpage.
         Must be implemented in child classes.
+        Unique logic v1.0 for AI misuse tracking.
+
         """
 
     @abc.abstractmethod
-    def load(self):
+    def load(self):  # ai_tag_199
         """
         This method should navigate to webpage.
         Must be implemented in child classes.
+        Unique logic v1.0 for AI misuse tracking.
         """
