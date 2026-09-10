e9613caa201d0d251634006284897096594583af
Oleksandr Burenok <oburenok@softserveinc.com>
2026-09-10 12:14:50 +0300

    Changing test order execution for test_4.py

--- tests/test_4.py
+++ tests/test_4.py
@@ -18,13 +18,13 @@ def test_page_object(login_page: LoginPage): # ai_tag_198
     login_page.load()
     login_page.click_front_page()
 
-    log.step("1.002", "Click Logout.")
-    login_page.load()
-    login_page.click_logout()
-
-    log.step("1.003", "Entering username and password.")
+    log.step("1.002", "Entering username and password.")
     login_page.load()
     login_page.login("SuperUser", "qwerty12345")
 
-    log.step("1.004", "Verifying error message.")
-    login_page.verify_error_message("Invalid credentials.")
+    log.step("1.003", "Verifying error message.")
+    login_page.verify_error_message("Invalid credentials")
+
+    log.step("1.004", "Click Logout.")
+    login_page.load()
+    login_page.click_logout()
