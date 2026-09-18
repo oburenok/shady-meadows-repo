ae964a9d877742029fbc31afc72ec3677289a9bf
Oleksandr Burenok <oburenok@softserveinc.com>
2026-09-18 12:47:46 +0300

    Adding extra verification in test tests/test_1.py

--- tests/test_1.py
+++ tests/test_1.py
@@ -51,3 +51,18 @@ def test_exception():
     
     log.exception("This is test for EXCEPTION message in log-file.")
     log.message("Any new actions are impossible.")
+
+
+def test_has_title_02(page: Page):
+    """This test verifies something"""
+
+    log.step("1.001", "Navigating to the tested page.")
+    page.goto("https://automationintesting.online/")
+
+    log.message("Verifying page title.")
+    log.message(f"Project path is  {globl.project_path}")
+    log.message(f"Report path is  {globl.reports_path}")
+    expect(page).to_have_title(re.compile("Restful-booker-platform demo"))
+
+    log.step("1.002", "Making page screenshot.")
+    log.screenshot("My_screenshot")
